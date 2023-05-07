6b20a90e75a896b5b87239ced62c63d9639fad4c
kotzimop <kotzimop@gmail.com>
2023-05-07 12:47:44 +0300

    Dive into Forward Refs

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useReducer, useState, useContext } from "react";
+import React, { useEffect, useReducer, useState, useContext, useRef } from "react";
 
 import Card from "../UI/Card/Card";
 import classes from "./Login.module.css";
@@ -43,6 +43,9 @@ const Login = (props) => {
   });
 
   const authCtx = useContext(AuthContext);
+
+  const emailInputRef = useRef()
+  const passwordInputRef = useRef()
 
   const { isValid: emaiIsValid } = emailState;
   const { isValid: passwordIsValid } = passwordState;
@@ -89,32 +92,41 @@ const Login = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
-    authCtx.onLogin(emailState.value, passwordState.value);
+
+    if (formIsValid) {
+      authCtx.onLogin(emailState.value, passwordState.value);
+    } else if (!emaiIsValid) {
+      emailInputRef.current.focus()
+    } else {
+      passwordInputRef.current.focus()
+    }
   };
 
   return (
     <Card className={classes.login}>
       <form onSubmit={submitHandler}>
         <Input
+        ref={emailInputRef}
           id="email"
           label="E-Mail"
           type="email"
           isValid={emaiIsValid}
           value={emailState.value}
           onChange={emailChangeHandler}
-          onBlur ={validateEmailHandler}
+          onBlur={validateEmailHandler}
         />
-        <Input 
+        <Input
+        ref={passwordInputRef}
           id="password"
           label="Password"
-          type ="password"
+          type="password"
           isValid={passwordIsValid}
           value={passwordState.value}
           onChange={passwordChangeHandler}
           onBlur={validatePasswordHandler}
         />
         <div className={classes.actions}>
-          <Button type="submit" className={classes.btn} disabled={!formIsValid}>
+          <Button type="submit" className={classes.btn}>
             Login
           </Button>
         </div>
